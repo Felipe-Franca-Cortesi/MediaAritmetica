5484a96e7653de775cd5e8608135903b86ddf246
Felipe França <contatofelipefranca12@gmail.com>
2020-09-28 12:12:15 -0300

    Resolução do exercício MediaAritmetica

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MediaAritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("--- Média aritmética de 3 números ---");
+            Console.Write("Informe o primeiro número: ");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe o segundo número: ");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe o terceiro número: ");
+            double terceiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            double media = (primeiroNumero + segundoNumero + terceiroNumero) / 3;
+            Console.WriteLine($"Média: {media}");
+            Console.Write("Pressione qualquer tecla para sair");
+            Console.ReadKey();
+        }
+    }
+}
